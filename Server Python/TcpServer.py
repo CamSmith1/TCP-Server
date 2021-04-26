01c198b0b580085f51cec0afac60a3ad8bf5d5eb
Cameron <Camsmith@camsmith.co.nz>
2021-04-26 20:49:54 +1200

    update service code

--- Server Python/TcpServer.py
+++ Server Python/TcpServer.py
@@ -15,10 +15,36 @@ def threaded(c):
             # lock released on exit
             print_lock.release()
             break
-        print("Sensor sent the data : {}".format(data))
-        file = open('/home/ubuntu/SensorLogs.txt', 'a')
-        file.write(format(data) + '\n')
-        file.close()
+
+        # start reading data
+        dataArr = data.decode('utf-8')
+        dataArr = list(dataArr)
+        reportType = dataArr[6] + dataArr[7]
+
+        # If the report type is 02 store all the data in DB
+        if reportType == "02":
+            packetSize = dataArr[8] + dataArr[9]
+            height = dataArr[10] + dataArr[11] + dataArr[12] + dataArr[13]
+            temperature = dataArr[14] + dataArr[15] + dataArr[16] + dataArr[17]
+            batteryVoltage = dataArr[26] + dataArr[27] + dataArr[28] + dataArr[29]
+            RSPR = dataArr[30] + dataArr[31] + dataArr[32] + dataArr[33] + dataArr[34] + dataArr[35] + dataArr[36] + dataArr[37]
+            FRAM = dataArr[38] + dataArr[39] + dataArr[40] + dataArr[41]
+            IMEI = dataArr[42] + dataArr[43] + dataArr[44] + dataArr[45] + dataArr[6] + dataArr[47] + dataArr[48] + dataArr[49] + dataArr[50] + dataArr[51] + dataArr[52] + dataArr[53] + dataArr[54] + dataArr[55] + dataArr[56] + dataArr[57]
+
+            print("packetSize " + packetSize)
+            print("Height " + height)
+            print("temperature " + temperature)
+            print("batteryVoltage " + batteryVoltage)
+            print("RSPR " + RSPR)
+            print("FRAM " + FRAM)
+            print("IMEI " + IMEI)
+            break
+        data = data[::-1]
+        # send back reversed string to client
+        c.send(data)
+        #file = open('/home/ubuntu/SensorLogs.txt', 'a')
+        #file.write(format(data) + '\n')
+        #file.close()
 
     c.close()
 
